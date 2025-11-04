08e76b7a4a27191d9ce1960b4d925aed2f1c1082
Виктор Кибакин <v-kibakin@yandex.ru>
2025-11-04 18:01:30 +0300

    add culculating birthday

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,11 +1,22 @@
+# birthday/views.py 
 from django.shortcuts import render
 
 from .forms import BirthdayForm
+# Импортируем из utils.py функцию для подсчёта дней.
+from .utils import calculate_birthday_countdown
 
 
 def birthday(request):
     form = BirthdayForm(request.GET or None)
-    if form.is_valid():
-        pass
+    # Создаём словарь контекста сразу после инициализации формы.
     context = {'form': form}
+    # Если форма валидна...
+    if form.is_valid():
+        # ...вызовем функцию подсчёта дней:
+        birthday_countdown = calculate_birthday_countdown(
+            # ...и передаём в неё дату из словаря cleaned_data.
+            form.cleaned_data['birthday']
+        )
+        # Обновляем словарь контекста: добавляем в него новый элемент.
+        context.update({'birthday_countdown': birthday_countdown})
     return render(request, 'birthday/birthday.html', context)
