4a8f494d93b43b426ebfc3087305d1a585a85f60
Виктор Кибакин <v-kibakin@yandex.ru>
2025-11-04 15:12:44 +0300

    add class for birthday form

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,1 +1,8 @@
+# birthday/forms.py
 from django import forms
+
+
+class BirthdayForm(forms.Form):
+    first_name = forms.CharField(max_length=20)
+    last_name = forms.CharField(required=False)
+    birthday = forms.DateField()
