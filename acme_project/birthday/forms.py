7e957306a66af5226c62ae78e5e737e694741660
Виктор Кибакин <v-kibakin@yandex.ru>
2025-11-05 13:24:03 +0300

    add birthday model

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -3,16 +3,11 @@ from django import forms
 
 
 class BirthdayForm(forms.Form):
-    first_name = forms.CharField(
-        label='Имя',
-        max_length=20
-    )
+    first_name = forms.CharField(label='Имя', max_length=20)
     last_name = forms.CharField(
-        label='Фамилия',
-        required=False,
-        help_text='Необязательное поле'
+        label='Фамилия', required=False, help_text='Необязательное поле'
     )
     birthday = forms.DateField(
         label='Дата рождения',
-        widget=forms.DateInput(attrs={'type': 'date'})
-    )
+        widget=forms.DateInput(attrs={'type': 'date'}),
+    ) 
