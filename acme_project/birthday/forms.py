8b3d5db4a3d61ec341bd8ca5020cc97e86d9f0b5
Виктор Кибакин <v-kibakin@yandex.ru>
2025-11-07 15:35:40 +0300

    add check for unique

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,7 +1,9 @@
 # birthday/forms.py
 from django import forms
-
+from django.core.exceptions import ValidationError
 from .models import Birthday
+
+BEATLES = {'Джон Леннон', 'Пол Маккартни', 'Джордж Харрисон', 'Ринго Старр'}
 
 
 class BirthdayForm(forms.ModelForm):
@@ -24,3 +26,14 @@ class BirthdayForm(forms.ModelForm):
         # Разбиваем полученную строку по пробелам 
         # и возвращаем только первое имя.
         return first_name.split()[0]
+
+    def clean(self):
+        super().clean()
+        # Получаем имя и фамилию из очищенных полей формы.
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        # Проверяем вхождение сочетания имени и фамилии во множество имён.
+        if f'{first_name} {last_name}' in BEATLES:
+            raise ValidationError(
+                'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
+            )
--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -9,3 +9,11 @@ class Birthday(models.Model):
         'Фамилия', blank=True, help_text='Необязательное поле', max_length=20
     )
     birthday = models.DateField('Дата рождения', validators=(real_age,))
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                flields=('first_name', 'last_name', 'birthday'),
+                name='Unique person constraint'
+            )
+        )
