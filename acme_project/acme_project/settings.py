879eb1e3382d57c6331d2e6ce269dc0ac0283598
Виктор Кибакин <v-kibakin@yandex.ru>
2025-11-07 16:49:40 +0300

    add directory for files

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -34,6 +34,8 @@ ROOT_URLCONF = 'acme_project.urls'
 
 TEMPLATES_DIR = BASE_DIR / 'templates'
 
+MEDIA_ROOT = BASE_DIR / 'media'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
