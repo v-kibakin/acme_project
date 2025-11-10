056091e5745557b1df2f193634f80e70fd967dff
Виктор Кибакин <v-kibakin@yandex.ru>
2025-11-10 11:27:31 +0300

    add mixin for better code str

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -9,19 +9,23 @@ from .models import Birthday
 from .utils import calculate_birthday_countdown
 
 
-class BirthdayUpdateView(UpdateView):
+# Создаём миксин.
+class BirthdayMixin:
     model = Birthday
     form_class = BirthdayForm
     template_name = 'birthday/birthday.html'
     success_url = reverse_lazy('birthday:list')
 
 
-class BirthdayCreateView(CreateView):
-    model = Birthday
-    # Указываем имя формы:
-    form_class = BirthdayForm
-    template_name = 'birthday/birthday.html'
-    success_url = reverse_lazy('birthday:list')
+# Добавляем миксин первым по списку родительских классов.
+class BirthdayCreateView(BirthdayMixin, CreateView):
+    # Не нужно описывать атрибуты: все они унаследованы от BirthdayMixin.
+    pass
+
+
+class BirthdayUpdateView(BirthdayMixin, UpdateView):
+    # И здесь все атрибуты наследуются от BirthdayMixin.
+    pass
 
 
 # Наследуем класс от встроенного ListView:
