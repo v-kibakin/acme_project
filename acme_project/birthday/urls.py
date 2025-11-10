7db68d72b0fc59eb5d74dcb334311a6fabafa52a
Виктор Кибакин <v-kibakin@yandex.ru>
2025-11-10 11:25:32 +0300

    change edit to CBV

--- acme_project/birthday/urls.py
+++ acme_project/birthday/urls.py
@@ -7,6 +7,6 @@ app_name = 'birthday'
 urlpatterns = [
     path('', views.BirthdayCreateView.as_view(), name='create'),
     path('list/', views.BirthdayListView.as_view(), name='list'),
-    path('<int:pk>/edit', views.birthday, name='edit'),
+    path('<int:pk>/edit', views.BirthdayUpdateView.as_view(), name='edit'),
     path('<int:pk>/delete/', views.delete_birthday, name='delete'),
 ]
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,6 +1,6 @@
 # birthday/views.py
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, UpdateView
 from django.urls import reverse_lazy
 
 from .forms import BirthdayForm
@@ -9,34 +9,11 @@ from .models import Birthday
 from .utils import calculate_birthday_countdown
 
 
-# Добавим опциональный параметр pk.
-def birthday(request, pk=None):
-    # Если в запросе указан pk (если получен запрос на редактирование объекта):
-    if pk is not None:
-        # Получаем объект модели или выбрасываем 404 ошибку.
-        instance = get_object_or_404(Birthday, pk=pk)
-    # Если в запросе не указан pk
-    # (если получен запрос к странице создания записи):
-    else:
-        # Связывать форму с объектом не нужно, установим значение None.
-        instance = None
-    # Передаём в форму либо данные из запроса, либо None. 
-    # В случае редактирования прикрепляем объект модели.
-    form = BirthdayForm(
-        request.POST or None,
-        files=request.FILES or None,
-        instance=instance
-    )
-    # Остальной код без изменений.
-    context = {'form': form}
-    # Сохраняем данные, полученные из формы, и отправляем ответ:
-    if form.is_valid():
-        form.save()
-        birthday_countdown = calculate_birthday_countdown(
-            form.cleaned_data['birthday']
-        )
-        context.update({'birthday_countdown': birthday_countdown})
-    return render(request, 'birthday/birthday.html', context)
+class BirthdayUpdateView(UpdateView):
+    model = Birthday
+    form_class = BirthdayForm
+    template_name = 'birthday/birthday.html'
+    success_url = reverse_lazy('birthday:list')
 
 
 class BirthdayCreateView(CreateView):
