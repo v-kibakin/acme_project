4da840ddce90c382b21430bdf5ad50d38cfddd6a
Виктор Кибакин <v-kibakin@yandex.ru>
2025-11-10 12:51:30 +0300

    making code prieatier

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,11 +1,18 @@
 # birthday/views.py
-from django.views.generic import CreateView, DetailView, DeleteView, ListView, UpdateView
+from django.views.generic import (
+    CreateView, DeleteView, DetailView, ListView, UpdateView
+)
 from django.urls import reverse_lazy
 
 from .forms import BirthdayForm
 from .models import Birthday
-# Импортируем из utils.py функцию для подсчёта дней.
 from .utils import calculate_birthday_countdown
+
+
+class BirthdayListView(ListView):
+    model = Birthday
+    ordering = 'id'
+    paginate_by = 10
 
 
 class BirthdayMixin:
@@ -13,43 +20,24 @@ class BirthdayMixin:
     success_url = reverse_lazy('birthday:list')
 
 
-class BirthdayFormMixin:
+class BirthdayCreateView(BirthdayMixin, CreateView):
     form_class = BirthdayForm
-    template_name = 'birthday/birthday.html'
 
 
-class BirthdayCreateView(BirthdayMixin, BirthdayFormMixin, CreateView):
-    pass
-
-
-class BirthdayUpdateView(BirthdayMixin, BirthdayFormMixin, UpdateView):
-    pass
+class BirthdayUpdateView(BirthdayMixin, UpdateView):
+    form_class = BirthdayForm
 
 
 class BirthdayDeleteView(BirthdayMixin, DeleteView):
     pass
 
 
-# Наследуем класс от встроенного ListView:
-class BirthdayListView(ListView):
-    # Указываем модель, с которой работает CBV...
-    model = Birthday
-    # ...сортировку, которая будет применена при выводе списка объектов:
-    ordering = 'id'
-    # ...и даже настройки пагинации:
-    paginate_by = 10
-
-
 class BirthdayDetailView(DetailView):
     model = Birthday
 
     def get_context_data(self, **kwargs):
-        # Получаем словарь контекста:
         context = super().get_context_data(**kwargs)
-        # Добавляем в словарь новый ключ:
         context['birthday_countdown'] = calculate_birthday_countdown(
-            # Дату рождения берём из объекта в словаре context:
             self.object.birthday
         )
-        # Возвращаем словарь контекста.
         return context
