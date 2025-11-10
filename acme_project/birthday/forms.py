903882d18ad141d083c88c40f1bef39b134e9b2b
Виктор Кибакин <v-kibakin@yandex.ru>
2025-11-10 17:16:18 +0300

    add test email message

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,6 +1,7 @@
 # birthday/forms.py
 from django import forms
 from django.core.exceptions import ValidationError
+from django.core.mail import send_mail
 from .models import Birthday
 
 BEATLES = {'Джон Леннон', 'Пол Маккартни', 'Джордж Харрисон', 'Ринго Старр'}
@@ -29,11 +30,18 @@ class BirthdayForm(forms.ModelForm):
 
     def clean(self):
         super().clean()
-        # Получаем имя и фамилию из очищенных полей формы.
         first_name = self.cleaned_data['first_name']
         last_name = self.cleaned_data['last_name']
-        # Проверяем вхождение сочетания имени и фамилии во множество имён.
         if f'{first_name} {last_name}' in BEATLES:
+            # Отправляем письмо, если кто-то представляется 
+            # именем одного из участников Beatles.
+            send_mail(
+                subject='Another Beatles member',
+                message=f'{first_name} {last_name} пытался опубликовать запись!',
+                from_email='birthday_form@acme.not',
+                recipient_list=['admin@acme.not'],
+                fail_silently=True,
+            )
             raise ValidationError(
                 'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
-            )
+            ) 
