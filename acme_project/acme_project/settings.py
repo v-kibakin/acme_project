ef2ade894af4f675cab3b7c1e67c4c34f120e100
Виктор Кибакин <v-kibakin@yandex.ru>
2025-11-12 15:30:55 +0300

    turn debug back on

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-m&$lzdzkutvrbr5vt=jpm)7#g7cken_tk%($ty+w902n7wb#=e'
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
