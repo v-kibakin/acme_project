e8f1debf8c3dc1de464daebcf7ddb7917af9fb57
Виктор Кибакин <v-kibakin@yandex.ru>
2025-11-12 11:26:28 +0300

    add registration page

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
-from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+# К импортам из django.urls добавьте импорт функции reverse_lazy
+from django.urls import include, path, reverse_lazy
+
 
 urlpatterns = [
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('birthday/', include('birthday.urls')),
     path('auth/', include('django.contrib.auth.urls')),
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('pages:homepage'),
+        ),
+        name='registration',
+    )
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
