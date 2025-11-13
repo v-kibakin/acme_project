6164dd15a5fcf9d2b9eff62fa715faed8dc63445
Виктор Кибакин <v-kibakin@yandex.ru>
2025-11-13 11:06:00 +0300

    add model and field for tags

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -7,6 +7,10 @@ from .validators import real_age
 User = get_user_model()
 
 
+class Tag(models.Model):
+    tag = models.CharField('Тег', max_length=20)
+
+
 class Birthday(models.Model):
     first_name = models.CharField('Имя', max_length=20)
     last_name = models.CharField(
@@ -20,6 +24,12 @@ class Birthday(models.Model):
         verbose_name='Автор записи',
         on_delete=models.CASCADE,
         null=True
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        verbose_name='Теги',
+        blank=True,
+        help_text='Удерживайте Ctrl для выбора нескольких вариантов'
     )
 
     class Meta:
